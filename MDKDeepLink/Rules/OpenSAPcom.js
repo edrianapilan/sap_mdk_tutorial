572ddbf5f9a7ae90a8b82736205a02f6881a7731
Edrian Apilan <edrian.apilan@accenture.com>
2023-05-09 10:50:19 +0000

    SAP MDK TUTORIAL

---
+++ MDKDeepLink/Rules/OpenSAPcom.js
@@ -0,0 +1,17 @@
+/**
+ * Describe this function...
+ * @param {IClientAPI} clientAPI
+ */
+export default function OpenSAPcom(context) {
+    // Get the Nativescript Utils Module
+    const utilsModule = context.nativescript.utilsModule;
+    return context.executeAction('/MDKDeepLink/Actions/Confirmation.action').then((result) => {
+        if (result.data) {
+            //This will open SAP.com website
+            return utilsModule.openUrl("https://www.sap.com");
+        } else {
+            return Promise.reject('User Deferred');
+        }
+    });
+}
+
